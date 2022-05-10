3decaf61d3f3524cbe21fba2045fd467b2f463de
0x00h <10893384+0x00h@users.noreply.github.com>
2022-05-10 03:06:21 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo from '../img/logo.png'
+import logo from '../img/logo1.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
