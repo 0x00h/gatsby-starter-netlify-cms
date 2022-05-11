6bf652dba1d11ef9db15de403871b232c61a1b8d
0x00h <10893384+0x00h@users.noreply.github.com>
2022-05-11 02:10:43 +0000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -59,7 +59,7 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://facebook.com/seefcafe">
                   <img
                     src={facebook}
                     alt="Facebook"
@@ -74,7 +74,7 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com/seefco">
+                <a title="instagram" href="https://instagram.com/seefcafe">
                   <img
                     src={instagram}
                     alt="Instagram"
